8588c08809d0ba44f89675a7585766673819b338
dabhivaibhav <vaibhavdabhi063@gmail.com>
2025-06-18 23:36:44 -0500

    first commit

---
+++ nexgennetwork/src/main/java/com/connectsphere/nexgennetwork/NexgennetworkApplication.java
@@ -0,0 +1,13 @@
+package com.connectsphere.nexgennetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexgennetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexgennetworkApplication.class, args);
+	}
+
+}
---
+++ nexgennetwork/src/test/java/com/connectsphere/nexgennetwork/NexgennetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectsphere.nexgennetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexgennetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
